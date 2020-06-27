db60d340756e95972a6bdeae9302dc22e23bcc14
layan-alruwayeh <66646994+layan-alruwayeh@users.noreply.github.com>
2020-06-27 21:16:07 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,32 @@
 // السؤال الأول
-// 1
-
+// 1  
+const name="ليان"
 // 2
-
+let age=16
 // 3 and 4
-
+console.logo( "انا" + name + "وعمري" + age )
 // 5 and 6
-
+console.logo(age*5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
-// 1
-
+// 1 
+let hobbies=["singing","horse riding","cooking","playing the piano"]
 // 2
-
+hobbies.push=(16)
 // 3
-
+hobbies.pop(age)
 // 4
-
+let web={
+        track="web",
+        language ="java script"
+};
 // 5
-
+let teachers= ta=["mohdi","nancy","rahaf","hussain","menawer","somou","mohamad" ]
 // 6
-
+console.logo(web);
 // 7
-
+console.logo(teacher [1])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
